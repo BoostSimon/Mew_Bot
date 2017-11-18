90aa6e53dfa01a5394cb9f0d8bb067ee207c54e4
BoostSimon <33784229+BoostSimon@users.noreply.github.com>
2017-11-18 23:23:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,8 +14,8 @@ client.on('message', message => {
   	}
     if (message.content === prefix + '.Commands') {
         message.reply("Commands list");
-        message.reply("m.Hello - say hello to the bot");
-        message.reply("m.TellMeAJoke - telling you a joke");
+        message.send("m.Hello - say hello to the bot");
+        message.send("m.TellMeAJoke - telling you a joke");
     }
     if (message.content === prefix + ".TellMeAJoke") {
        message.reply("Robox is a good game");   
